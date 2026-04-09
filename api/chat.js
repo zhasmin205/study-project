ff64b59d1169388ccc51c68e15baa9233e761ed6
Alinur Seisekov <alinur.seysekov@gmail.com>
2026-04-10 01:17:48 +0500

    Fix serverless function export syntax

--- api/chat.js
+++ api/chat.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   // Enable CORS
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
